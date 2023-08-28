db7ff0a24d49974bfa6542743d13395a502b5138
Paul Sanders <psanders1@gmail.com>
2023-08-27 22:51:32 -0400

    Add ci testing files

--- src/project_generator.rs
+++ src/project_generator.rs
@@ -189,6 +189,225 @@ TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
     Ok(())
 }
 
+fn create_ci_testing_linux_only_file(
+    project_slug: &str,
+    source_dir: &str,
+    min_python_version: &str,
+    github_action_python_test_versions: &[String],
+) -> Result<()> {
+    let file_path = format!("{project_slug}/.github/workflows/testing.yml");
+    let python_versions = github_action_python_test_versions
+        .iter()
+        .map(|x| format!(r#""{x}""#))
+        .collect::<Vec<String>>()
+        .join(", ");
+    let content = format!(
+        r#"name: Testing
+
+on:
+  push:
+    branches:
+    - main
+  pull_request:
+jobs:
+  linting:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v3
+    - name: Set up Python
+      uses: actions/setup-python@v4
+      with:
+        python-version: "{min_python_version}"
+    - name: Get full Python version
+      id: full-python-version
+      run: echo version=$(python -c "import sys; print('-'.join(str(v) for v in sys.version_info))") >> $GITHUB_OUTPUT
+    - name: Install Poetry
+      run: |
+        pip install pipx
+        pipx install poetry
+    - name: Configure poetry
+      run: |
+        poetry config virtualenvs.create true
+        poetry config virtualenvs.in-project true
+    - name: Cache poetry venv
+      uses: actions/cache@v3
+      id: poetry-cache
+      with:
+        path: .venv
+        key: venv-${{{{ runner.os }}}}-${{{{ steps.full-python-version.outputs.version }}}}-${{{{ hashFiles('**/poetry.lock') }}}}
+    - name: Ensure cache is healthy
+      if: steps.poetry-cache.outputs.cache-hit == 'true'
+      shell: bash
+      run: timeout 10s poetry run pip --version || rm -rf .venv
+    - name: Install Dependencies
+      run: poetry install
+    - name: Black check
+      run: |
+        poetry run black {source_dir} tests --check
+    - name: Lint with ruff
+      run: |
+        poetry run ruff check .
+    - name: mypy check
+      run: |
+        poetry run mypy .
+
+  testing:
+    strategy:
+      fail-fast: false
+      matrix:
+        python-version: [{python_versions}]
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v3
+    - name: Set up Python {{{{ matrix.python-version }}}}
+      uses: actions/setup-python@v4
+      with:
+        python-version: {{{{ matrix.python-version }}}}
+    - name: Get full Python version
+      id: full-python-version
+      run: echo version=$(python -c "import sys; print('-'.join(str(v) for v in sys.version_info))") >> $GITHUB_OUTPUT
+    - name: Install Poetry
+      run: |
+        pip install pipx
+        pipx install poetry
+    - name: Configure poetry
+      run: |
+        poetry config virtualenvs.create true
+        poetry config virtualenvs.in-project true
+    - name: Cache poetry venv
+      uses: actions/cache@v3
+      id: poetry-cache
+      with:
+        path: .venv
+        key: venv-${{{{ runner.os }}}}-${{{{ steps.full-python-version.outputs.version }}}}-${{{{ hashFiles('**/poetry.lock') }}}}
+    - name: Ensure cache is healthy
+      if: steps.poetry-cache.outputs.cache-hit == 'true'
+      shell: bash
+      run: timeout 10s poetry run pip --version || rm -rf .venv
+    - name: Install Dependencies
+      run: poetry install
+    - name: Test with pytest
+      run: |
+        poetry run pytest
+
+"#
+    );
+
+    create_file_with_content(&file_path, &content)?;
+
+    Ok(())
+}
+
+fn create_ci_testing_multi_os_file(
+    project_slug: &str,
+    source_dir: &str,
+    min_python_version: &str,
+    github_action_python_test_versions: &[String],
+) -> Result<()> {
+    let file_path = format!("{project_slug}/.github/workflows/testing.yml");
+    let python_versions = github_action_python_test_versions
+        .iter()
+        .map(|x| format!(r#""{x}""#))
+        .collect::<Vec<String>>()
+        .join(", ");
+    let content = format!(
+        r#"name: Testing
+
+on:
+  push:
+    branches:
+    - main
+  pull_request:
+jobs:
+  linting:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v3
+    - name: Set up Python
+      uses: actions/setup-python@v4
+      with:
+        python-version: "{min_python_version}"
+    - name: Get full Python version
+      id: full-python-version
+      run: echo version=$(python -c "import sys; print('-'.join(str(v) for v in sys.version_info))") >> $GITHUB_OUTPUT
+    - name: Install Poetry
+      run: |
+        pip install pipx
+        pipx install poetry
+    - name: Configure poetry
+      run: |
+        poetry config virtualenvs.create true
+        poetry config virtualenvs.in-project true
+    - name: Cache poetry venv
+      uses: actions/cache@v3
+      id: poetry-cache
+      with:
+        path: .venv
+        key: venv-${{{{ runner.os }}}}-${{{{ steps.full-python-version.outputs.version }}}}-${{{{ hashFiles('**/poetry.lock') }}}}
+    - name: Ensure cache is healthy
+      if: steps.poetry-cache.outputs.cache-hit == 'true'
+      shell: bash
+      run: timeout 10s poetry run pip --version || rm -rf .venv
+    - name: Install Dependencies
+      run: poetry install
+    - name: Black check
+      run: |
+        poetry run black {source_dir} tests --check
+    - name: Lint with ruff
+      run: |
+        poetry run ruff check .
+    - name: mypy check
+      run: |
+        poetry run mypy .
+
+  testing:
+    strategy:
+      fail-fast: false
+      matrix:
+        python-version: [{python_versions}]
+        os: [ubuntu-latest, windows-latest, macos-latest]
+    runs-on: {{{{matrix.os}}}}
+    steps:
+    - uses: actions/checkout@v3
+    - name: Set up Python {{{{ matrix.python-version }}}}
+      uses: actions/setup-python@v4
+      with:
+        python-version: {{{{ matrix.python-version }}}}
+    - name: Get full Python version
+      id: full-python-version
+      run: echo version=$(python -c "import sys; print('-'.join(str(v) for v in sys.version_info))") >> $GITHUB_OUTPUT
+    - name: Install Poetry
+      run: |
+        pip install pipx
+        pipx install poetry
+    - name: Configure poetry
+      run: |
+        poetry config virtualenvs.create true
+        poetry config virtualenvs.in-project true
+    - name: Cache poetry venv
+      uses: actions/cache@v3
+      id: poetry-cache
+      with:
+        path: .venv
+        key: venv-${{{{ runner.os }}}}-${{{{ steps.full-python-version.outputs.version }}}}-${{{{ hashFiles('**/poetry.lock') }}}}
+    - name: Ensure cache is healthy
+      if: steps.poetry-cache.outputs.cache-hit == 'true'
+      shell: bash
+      run: timeout 10s poetry run pip --version || rm -rf .venv
+    - name: Install Dependencies
+      run: poetry install
+    - name: Test with pytest
+      run: |
+        poetry run pytest
+
+"#
+    );
+
+    create_file_with_content(&file_path, &content)?;
+
+    Ok(())
+}
+
 fn create_dependabot_file(project_slug: &str) -> Result<()> {
     let file_path = format!("{project_slug}/.github/dependabot.yml");
     let content = r#"version: 2
@@ -892,19 +1111,43 @@ pub fn generate_project(project_info: &ProjectInfo) {
         std::process::exit(1);
     }
 
-    if project_info.use_dependabot {
-        if create_dependabot_file(&project_info.project_slug).is_err() {
-            let error_message = "Error creating dependabot file";
+    if project_info.use_multi_os_ci {
+        if create_ci_testing_multi_os_file(
+            &project_info.project_slug,
+            &project_info.source_dir,
+            &project_info.min_python_version,
+            &project_info.github_action_python_test_versions,
+        )
+        .is_err()
+        {
+            let error_message = "Error creating CI teesting file";
             println!("\n{}", error_message.red());
             std::process::exit(1);
         }
+    } else if create_ci_testing_linux_only_file(
+        &project_info.project_slug,
+        &project_info.source_dir,
+        &project_info.min_python_version,
+        &project_info.github_action_python_test_versions,
+    )
+    .is_err()
+    {
+        let error_message = "Error creating CI teesting file";
+        println!("\n{}", error_message.red());
+        std::process::exit(1);
     }
 
-    if project_info.use_release_drafter {
-        if create_release_drafter_file(&project_info.project_slug).is_err() {
-            let error_message = "Error creating release drafter file";
-            println!("\n{}", error_message.red());
-            std::process::exit(1);
-        }
+    if project_info.use_dependabot && create_dependabot_file(&project_info.project_slug).is_err() {
+        let error_message = "Error creating dependabot file";
+        println!("\n{}", error_message.red());
+        std::process::exit(1);
+    }
+
+    if project_info.use_release_drafter
+        && create_release_drafter_file(&project_info.project_slug).is_err()
+    {
+        let error_message = "Error creating release drafter file";
+        println!("\n{}", error_message.red());
+        std::process::exit(1);
     }
 }
