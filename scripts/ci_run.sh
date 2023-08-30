079c6f6d30547afaaa75927b238de50b8903ad5a
Paul Sanders <psanders1@gmail.com>
2023-08-30 10:49:24 -0400

    Test application and lib

--- scripts/ci_run.sh
+++ scripts/ci_run.sh
@@ -13,6 +13,17 @@ python_version=""
 min_python_version=""
 gha_versions=""
 application=""
+
+# Check for user provided application input
+if [ $# -gt 0 ]; then
+  if [ $1 -lt 1 ] || [ $1 -gt 2 ]; then
+    echo "Invalid application value"
+    exit 1
+  fi
+
+  application="$1"
+fi
+
 max_line_length=""
 use_dependabot=""
 use_continuous_deployment=""
