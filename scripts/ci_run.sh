6a9cec272123332b20a2001bcf9bab05f6d37ed4
Paul Sanders <psanders1@gmail.com>
2023-08-30 11:02:14 -0400

    Improve matrix

--- scripts/ci_run.sh
+++ scripts/ci_run.sh
@@ -16,12 +16,14 @@ application=""
 
 # Check for user provided application input
 if [ $# -gt 0 ]; then
-  if [ $1 -lt 1 ] || [ $1 -gt 2 ]; then
+  if [ $1 = "application" ]; then
+    application="1"
+  elif [ $1 = "lib" ]; then
+    application="2"
+  else
     echo "Invalid application value"
     exit 1
   fi
-
-  application="$1"
 fi
 
 max_line_length=""
