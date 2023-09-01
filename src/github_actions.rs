b3dd21c0d24de1bf2e17e3bc2850dd0e76ff0994
Paul Sanders <psanders1@gmail.com>
2023-09-01 16:55:38 -0400

    Fix testing yaml

--- src/github_actions.rs
+++ src/github_actions.rs
@@ -71,10 +71,10 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - uses: actions/checkout@v3
-    - name: Set up Python {{{{ matrix.python-version }}}}
+    - name: Set up Python ${{{{ matrix.python-version }}}}
       uses: actions/setup-python@v4
       with:
-        python-version: {{{{ matrix.python-version }}}}
+        python-version: ${{{{ matrix.python-version }}}}
     - name: Get full Python version
       id: full-python-version
       run: echo version=$(python -c "import sys; print('-'.join(str(v) for v in sys.version_info))") >> $GITHUB_OUTPUT
@@ -190,13 +190,13 @@ jobs:
       matrix:
         python-version: [{python_versions}]
         os: [ubuntu-latest, windows-latest, macos-latest]
-    runs-on: {{{{matrix.os}}}}
+    runs-on: ${{{{ matrix.os }}}}
     steps:
     - uses: actions/checkout@v3
-    - name: Set up Python {{{{ matrix.python-version }}}}
+    - name: Set up Python ${{{{ matrix.python-version }}}}
       uses: actions/setup-python@v4
       with:
-        python-version: {{{{ matrix.python-version }}}}
+        python-version: ${{{{ matrix.python-version }}}}
     - name: Get full Python version
       id: full-python-version
       run: echo version=$(python -c "import sys; print('-'.join(str(v) for v in sys.version_info))") >> $GITHUB_OUTPUT
@@ -443,10 +443,10 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - uses: actions/checkout@v3
-    - name: Set up Python {{ matrix.python-version }}
+    - name: Set up Python ${{ matrix.python-version }}
       uses: actions/setup-python@v4
       with:
-        python-version: {{ matrix.python-version }}
+        python-version: ${{ matrix.python-version }}
     - name: Get full Python version
       id: full-python-version
       run: echo version=$(python -c "import sys; print('-'.join(str(v) for v in sys.version_info))") >> $GITHUB_OUTPUT
@@ -548,13 +548,13 @@ jobs:
       matrix:
         python-version: ["3.8", "3.9", "3.10", "3.11"]
         os: [ubuntu-latest, windows-latest, macos-latest]
-    runs-on: {{matrix.os}}
+    runs-on: ${{ matrix.os }}
     steps:
     - uses: actions/checkout@v3
-    - name: Set up Python {{ matrix.python-version }}
+    - name: Set up Python ${{ matrix.python-version }}
       uses: actions/setup-python@v4
       with:
-        python-version: {{ matrix.python-version }}
+        python-version: ${{ matrix.python-version }}
     - name: Get full Python version
       id: full-python-version
       run: echo version=$(python -c "import sys; print('-'.join(str(v) for v in sys.version_info))") >> $GITHUB_OUTPUT
