86d44ad251f19eabca1ff79e9a8175a25ec08d95
Paul Sanders <psanders1@gmail.com>
2023-09-01 11:18:53 -0400

    Get latest versions for pre-commit

--- src/python_package_version.rs
+++ src/python_package_version.rs
@@ -1,24 +1,63 @@
 use anyhow::Result;
-use serde::Deserialize;
 
-pub trait PypiPackage {
-    fn get_latest_version(&self) -> Result<PythonPackageVersion>;
+#[derive(Debug)]
+pub enum PreCommitHook {
+    Black,
+    PreCommit,
+    MyPy,
+    Ruff,
 }
 
-#[derive(Debug, Deserialize)]
+pub trait LatestVersion {
+    fn get_latest_version(&mut self) -> Result<()>;
+}
+
+#[derive(Debug)]
+pub struct PreCommitHookVersion {
+    pub id: PreCommitHook,
+    pub repo: String,
+    pub rev: String,
+}
+
+impl LatestVersion for PreCommitHookVersion {
+    fn get_latest_version(&mut self) -> Result<()> {
+        let client = reqwest::blocking::Client::new();
+        let api_url = format!(
+            "{}/releases",
+            self.repo
+                .replace("https://github.com", "https://api.github.com/repos")
+        );
+        let response = client
+            .get(api_url)
+            .header(reqwest::header::USER_AGENT, "python-project-generator")
+            .send()?
+            .text()?;
+        let info: Vec<serde_json::Value> = serde_json::from_str(&response)?;
+        for i in info {
+            if i["draft"] == false && i["prerelease"] == false {
+                self.rev = i["tag_name"].to_string().replace('"', "");
+                break;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
 pub struct PythonPackageVersion {
     pub name: String,
     pub version: String,
 }
 
-impl PypiPackage for PythonPackageVersion {
-    fn get_latest_version(&self) -> Result<Self> {
+impl LatestVersion for PythonPackageVersion {
+    fn get_latest_version(&mut self) -> Result<()> {
         let url = format!("https://pypi.org/pypi/{}/json", self.name);
         let response = reqwest::blocking::get(url)?.text()?;
         let info: serde_json::Value = serde_json::from_str(&response)?;
-        let name = info["info"]["name"].to_string().replace('"', "");
-        let version = info["info"]["version"].to_string().replace('"', "");
+        self.name = info["info"]["name"].to_string().replace('"', "");
+        self.version = info["info"]["version"].to_string().replace('"', "");
 
-        Ok(Self { name, version })
+        Ok(())
     }
 }
