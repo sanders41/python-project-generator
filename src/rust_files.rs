23a685283e692da7b7b0c756e415a11b7082ea8e
Paul Sanders <paul@paulsanders.dev>
2024-03-25 21:22:12 -0400

    Update test

--- src/rust_files.rs
+++ src/rust_files.rs
@@ -218,7 +218,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {{
 }}
 
 #[pymodule]
-fn _{}(_py: Python, m: &PyModule) -> PyResult<()> {{
+fn _{}(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {{
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }}
