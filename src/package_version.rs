0308baa3d4c5d2723497fb917b26a6f494af9c63
Paul Sanders <paul@paulsanders.dev>
2024-09-05 13:40:26 -0400

    Bump default ruff version 0.6.3 -> 0.6.4

--- src/package_version.rs
+++ src/package_version.rs
@@ -155,7 +155,7 @@ pub fn default_version(package: &PythonPackage) -> String {
         PythonPackage::PreCommit => "3.8.0".to_string(),
         PythonPackage::Pytest => "8.3.2".to_string(),
         PythonPackage::PytestCov => "5.0.0".to_string(),
-        PythonPackage::Ruff => "0.6.3".to_string(),
+        PythonPackage::Ruff => "0.6.4".to_string(),
         PythonPackage::Tomli => "2.0.1".to_string(),
     }
 }
@@ -164,7 +164,7 @@ pub fn default_pre_commit_rev(hook: &PreCommitHook) -> String {
     match hook {
         PreCommitHook::MyPy => "v1.11.2".to_string(),
         PreCommitHook::PreCommit => "v4.6.0".to_string(),
-        PreCommitHook::Ruff => "v0.6.3".to_string(),
+        PreCommitHook::Ruff => "v0.6.4".to_string(),
     }
 }
 
