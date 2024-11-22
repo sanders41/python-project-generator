043b06f58426b8d04145f039ed8b741cf38b63f4
Paul Sanders <paul@paulsanders.dev>
2024-11-22 15:05:27 -0500

    Add unsorted __all__ and __slots__ check to ruff config

--- src/project_generator.rs
+++ src/project_generator.rs
@@ -777,6 +777,8 @@ select = [
   "I001",  # unsorted-imports
   "T201",  # print found
   "T203",  # pprint found
+  "RUFF022",  # Unsorted __all__
+  "RUFF023",  # Unforted __slots__
   {%- if is_async_project %}
   "ASYNC",  # flake8-async
   {% endif %}
