df248dbfd7da15c20181b3475d94e4ac20390156
Paul Sanders <paul@paulsanders.dev>
2025-07-26 14:05:40 -0400

    Bump default mkdocs-material version 9.6.15 -> 9.6.16

--- src/package_version.rs
+++ src/package_version.rs
@@ -168,7 +168,7 @@ pub fn default_version(package: &PythonPackage) -> String {
     match package {
         PythonPackage::Maturin => "1.9.1".to_string(),
         PythonPackage::Mkdocs => "1.6.1".to_string(),
-        PythonPackage::MkdocsMaterial => "9.6.15".to_string(),
+        PythonPackage::MkdocsMaterial => "9.6.16".to_string(),
         PythonPackage::Mkdocstrings => "0.30.0".to_string(),
         PythonPackage::MyPy => "1.17.0".to_string(),
         PythonPackage::PreCommit => "4.2.0".to_string(),
